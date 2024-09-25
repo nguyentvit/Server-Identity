bfa697f18d20d28b7f87ef9f8ed804cbc02e7cf5
nguyentvit <nguyentv.it@gmail.com>
2024-09-25 21:06:17 +0700

    helo

--- Identity.API/obj/Debug/net8.0/Identity.API.AssemblyInfo.cs
+++ Identity.API/obj/Debug/net8.0/Identity.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Identity.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37dc60d82599bbff980fe154a66ac08bfa9172f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6800387f1015d03d9d404d2a2265a25908c65741")]
 [assembly: System.Reflection.AssemblyProductAttribute("Identity.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Identity.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
